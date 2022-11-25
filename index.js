c69c0bf48270b54c5d9ba3b0d1cf9a7431ab83a1
¨kericho¨ <lvisrono90@gmail.com>
2022-11-25 15:58:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(action){
+    if (action == "bathe my dog")
+    return "This Saturday, I want to bathe my dog!"
+     else return "This Saturday, I want to roller-skate!"
+}
+function mondayWork(action = 'go to the office'){
+    if(action == 'work from home')
+    return "This Monday, I will work from home."
+    else return "This Monday, I will go to the office."
+}
+function wrapAdjective(join = "*"){
+    return function(word){
+        return `${join} ${word} ${join}`
+    }
+}
+function wrapAdjective(join= "||"){
+    return function(word= "a dedicated programmer"){
+        return`You are ${join}${word}${join}!`
+    }
+}
